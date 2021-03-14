aaacb69a7b04fe4e645162cf95430cbd302734b5
Allan Bruno <allanbrunorj@gmail.com>
2021-03-14 19:18:25 -0300

    deixando image de lado

--- bichos/models.py
+++ bichos/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     picture = models.ImageField(upload_to='fotos_bichos')
     author = models.CharField(max_length=50)
-    published_date = models.DateTimeField(blank=True, null=True)
+    # published_date = models.DateTimeField(blank=True, null=True)
     votes_up = models.IntegerField(default=0)
     votes_down = models.IntegerField(default=0)
 
